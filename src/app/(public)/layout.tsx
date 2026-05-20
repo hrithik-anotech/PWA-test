0c05be8e33024a9ba891d4c3b43e326f0307c862
hrithik-anotech <hrithik.gupta@anotechsolutions.com>
2026-05-20 11:26:16 +0530

    pwa test

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -11,7 +11,7 @@ export default function PublicLayout({
     <MobileShell>
       <PageTransitionProvider>
         <AppRefreshWrapper>
-          <div className="h-full min-h-0 flex-1 overflow-hidden">
+          <div className="h-full min-h-0 flex-1 overflow-hidden pt-4">
             {children}
           </div>
         </AppRefreshWrapper>
